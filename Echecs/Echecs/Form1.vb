b2b549c789f1778ab96418a60ca7641a9d799396
vbtheory <ImAVBG@gmail.com>
2014-11-18 17:41:08 +0000

    Edited project files
    Changes to animated PBX (color animation)

--- Echecs/Echecs/Form1.vb
+++ Echecs/Echecs/Form1.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        Dim max As Integer = Math.Max(Width, Height)
+        Width = max
+        Height = max
+    End Sub
 End Class
